69251491f3da7894811410fde0347246ab1a5180
AgungBahtiarr <agungbahtiar050@gmail.com>
2025-01-28 09:12:18 +0700

    feat: add sitemap integration and update site URL in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://laros.net.id",
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
 });
